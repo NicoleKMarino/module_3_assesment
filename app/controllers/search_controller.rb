dfa1eae0a2629a9c0f63a6d8da026d8a3f09fd2a
Nicole Marino <nicolekmarino@hotmail.com>
2016-09-20 10:31:07 -0600

    find stores method

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   def index
-    @stores = bb_services.find_stores(params[:zipcode])
-  end 
+    @stores = bb_services.find_stores(params[:zipcode])["stores"]
+  end
 
   def show
   end
